4ed10e546dc252381ac019f1e889a415ca6b2327
Fernando <ferdelamad@gmail.com>
2018-12-06 16:14:04 -0800

    Added Skew type Button

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Button01 from './Button01';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <h1>Hello world!</h1>
+        <Button01>Button</Button01>
       </div>
     );
   }
