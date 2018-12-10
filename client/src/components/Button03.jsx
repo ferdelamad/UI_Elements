e0d4f6cc02849cb8cedfd41dfd66ea53dd5e6aed
Fernando <ferdelamad@gmail.com>
2018-12-09 21:07:09 -0800

    Changed style of Button03

--- client/src/components/Button03.jsx
+++ client/src/components/Button03.jsx
@@ -1,26 +1,25 @@
 import styled from 'styled-components';
 
 const Button03 = styled.a`
-  position: absolute;
   top: 30%;
   left: 50%;
+  position: absolute;
   text-decoration: none;
   text-transform: uppercase;
   text-align: center;
   letter-spacing: 2px;
-  width: 100px;
-  height: 20px;
-  padding: 15px 20px;
-  border: 2px solid lightblue;
+  padding: 15px 30px;
+  border: 2px solid ${props => props.borderColor || 'rebeccapurple'};
   transform: scale(0.9);
   transition: 0.3s;
   font-family: Helvetica, sans-serif;
   font-size: 17px;
+  background: #fff;
   cursor: pointer;
 
   :hover {
     transform: scale(1);
-    background: lightblue;
+    background: ${props => props.background || 'rebeccapurple'};
     color: #fff;
     font-weight: 500;
   }
