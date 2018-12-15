1b6a158058a8230fc8fb07f01ebc70d59068edaf
Fernando <ferdelamad@gmail.com>
2018-12-14 18:45:00 -0800

    Added Button05 to the collection

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import Button01 from './Button01';
 import Button02 from './Button02';
 import Button03 from './Button03';
 import Button04 from './Button04';
+import Button05 from './Button05';
 
 const App = () => (
   <div className="container">
@@ -11,6 +12,7 @@ const App = () => (
     <Button02>Button</Button02>
     <Button03>Button</Button03>
     <Button04>Button</Button04>
+    <Button05>Button</Button05>
   </div>
 );
 
